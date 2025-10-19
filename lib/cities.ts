ce33236a9064ab1572b485a86a979c8d6519a21e
ahnshy <ahnshy@naver.com>
2025-10-19 09:41:54 +0900

    <feat> : cities Initial commit.

---
+++ lib/cities.ts
@@ -0,0 +1,11 @@
+export type City = { name: string; latitude: number; longitude: number };
+export const KOREAN_CITIES: City[] = [
+  { name: "서울 (Seoul)", latitude: 37.5665, longitude: 126.9780 },
+  { name: "부산 (Busan)", latitude: 35.1796, longitude: 129.0756 },
+  { name: "인천 (Incheon)", latitude: 37.4563, longitude: 126.7052 },
+  { name: "대구 (Daegu)", latitude: 35.8714, longitude: 128.6014 },
+  { name: "대전 (Daejeon)", latitude: 36.3504, longitude: 127.3845 },
+  { name: "광주 (Gwangju)", latitude: 35.1595, longitude: 126.8526 },
+  { name: "울산 (Ulsan)", latitude: 35.5384, longitude: 129.3114 },
+  { name: "제주 (Jeju)", latitude: 33.4996, longitude: 126.5312 }
+];
